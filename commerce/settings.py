709872f6e7d3b4c721f9ea84fa50df092516f8bc
Andrew <andrewhisshion@gmail.com>
2022-09-19 05:53:56 +0800

    test v.1.00

--- commerce/settings.py
+++ commerce/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 IS_HEROKU = "DYNO" in os.environ
 
 
-if 'SECRET_KEY' in os.environ:
+if "SECRET_KEY" in os.environ:
     SECRET_KEY = os.environ["SECRET_KEY"]
     
 if IS_HEROKU:
