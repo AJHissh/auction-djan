2217b739eafa1e1781262e94c9413435ee72418f
Andrew <andrewhisshion@gmail.com>
2022-09-19 06:10:16 +0800

    test v.1.00

--- commerce/settings.py
+++ commerce/settings.py
@@ -135,6 +135,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
